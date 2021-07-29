2373b78887f12572c794870a191b9fc54c52d414
Jenis Rai <jenishrai313@gmail.com>
2021-07-30 00:40:09 +1000

    Responsive hamburger menu project

---
+++ hamburger-menu/index.js
@@ -0,0 +1,16 @@
+const mainMenu = document.querySelector('.mainMenu')
+const closeMenu = document.querySelector('.closeMenu')
+const openMenu = document.querySelector('.openMenu')
+
+openMenu.addEventListener('click', show)
+closeMenu.addEventListener('click', hide)
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = 0;
+
+}
+function hide(){
+    // mainMenu.style.display = 'none';
+    mainMenu.style.top= '-100%';
+}
